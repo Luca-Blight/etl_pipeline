c9431670d3bee72954151afc2a814ec64a7c1cf9
royalszachary <royalszachary@penguin.lxd>
2021-10-26 23:28:38 -0400

    additional dataset added & dask library

--- adapter/builder.py
+++ adapter/builder.py
@@ -1,39 +1,44 @@
+from prefect import task, Flow
 from prefect.schedules.schedules import IntervalSchedule
+from prefect.engine.executors import DaskExecutor
+from dask.distributed import Client
 from sodapy import Socrata
 import pandas as pd
-import dask.dataframe as dd
-import os
-from prefect import task, Flow, Parameter
-from dotenv import load_dotenv
 import datetime as dt
+import os
+
 
 s3_bucket_path = os.getenv('S3_BASE_LOCATION')
 
-@task
+@task(nout=2)
 def extract():
     class Client:
-        APP_TOKEN = "XNKZJVsscuBPRteOXyyM6G3yc"
-        ROOT_URL = "data.cityofnewyork.us"
-        # END_POINT_ONE - School_Location Dataset
-        END_POINT_ONE = "8b6c-7uty" #"ahjc-fdu3" 
- 
+        APP_TOKEN = os.getenviron('APP_TOKEN')
+        ROOT_URL = os.getenviron('ROOT_URL')       
+        END_POINT_ONE = os.getenviron('END_POINT_ONE')
+        END_POINT_TWO = os.getenviron('END_POINT_TWO')
+
     
-        def request_school_locs(self):
-            client = Socrata(self.ROOT_URL, app_token = self.APP_TOKEN)
-            data = client.get(self.END_POINT_ONE, content_type = "json")
+        def request_school_info(self):
+            sub_client = Socrata(self.ROOT_URL, app_token = self.APP_TOKEN)
+            data = sub_client.get(self.END_POINT_ONE, content_type = "json")
             return data    
+        
+        def request_school_results(self):
+            sub_client = Socrata(self.ROOT_URL, app_token = self.APP_TOKEN)
+            data = sub_client.get(self.END_POINT_TWO, content_type = "json")
+            return data 
+            
 
     school_loc_details = Client().request_school_locs()
-    return school_loc_details
+    school_results = Client().request_school_locs()
+
+    return school_loc_details, school_results
 
 @task
-def create_df(records):
+def create_transform_df(records):
     df = pd.DataFrame(records)
     ddf = dd.from_pandas(df, npartitions=2)
-    return df
-
-@task
-def transform(df):
     columns = ['dbn','school_name','primary_address_line_1','city','postcode',
                'latitude','longitude','gradespan','subway','pct_stu_safe','graduation_rate',
                'attendance_rate','college_career_rate']
@@ -41,21 +46,30 @@ def transform(df):
     return transformed_df
 
 @task
+def create_transform_df_two(records):
+    df = pd.DataFrame(records)
+    ddf = dd.from_pandas(df, npartitions=2)
+    return df
+
+@task
+def merge(df_one,df_two):
+    merged_df = pd.merge(left=df_one,right=df_two,on='dbn')
+    return merged_df
+
+@task
 def load(transformed_df):
     #s3_bucket_path,src_of_data
- 
     loaded_object = transformed_df.to_csv('./out.csv', encoding='utf-8', index=False)
- 
     # loaded_object = transformed_df.to_parquet(s3_bucket_path + f"/{src_of_data}.parquet" ,engine='fastparquet')
     return loaded_object
 
-
 def build_flow(schedule=None):
     with Flow("my_etl",schedule=schedule) as flow:
-        data = extract()
-        df = create_df(data)
-        tdata = transform(df)
-        ldata = load(tdata)
+        data1,data2 = extract()
+        df_1 = create_transform_df(data1)
+        df_2 = create_transform_df_two(data2)
+        merged_df = merge(df_1,df_2)
+        tdata = load(merged_df)
     return flow
 
 
@@ -64,5 +78,7 @@ if __name__ == "__main__":
     schedule = IntervalSchedule(start_date=dt.datetime.now() + dt.timedelta(seconds=1),
                 interval=dt.timedelta(seconds=5))
     flow = build_flow(schedule)
+    flow.visualize()
     flow.run()
+
 
